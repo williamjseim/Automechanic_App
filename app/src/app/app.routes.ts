e7dec6a1d6226d2bc15f8f64366a3af9cfbed07e
williamjseim <33179905+williamjseim@users.noreply.github.com>
2024-05-22 12:15:41 +0200

    asd

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { VideoCaptureComponent } from './components/video-capture/video-capture.
 import { LogoutComponent } from './components/logout/logout.component';
 import { ErrorComponent } from './components/error/error.component';
 import { CreateCarFormComponent } from './components/create-car-form/create-car-form.component';
+import { CarPageComponent } from './components/car-page/car-page.component';
 
 export const routes: Routes = [
 
@@ -12,5 +13,6 @@ export const routes: Routes = [
  { path: 'record', component: VideoCaptureComponent},
  { path: 'logout', component: LogoutComponent },
  { path: 'createcar', component: CreateCarFormComponent },
+ { path: 'cars', component: CarPageComponent },
  { path: '**', component: ErrorComponent },
 ];
