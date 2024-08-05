e737904b3c18642dfe72a84deded70f0ec7c8717
Daniel Spurrell <danielspurrell@hotmail.com>
2024-08-05 10:49:28 +0200

    change issue status

---
+++ Backend/HackGame.Api/Migrations/20240805063139_IssueCompletion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mechanic.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IssueCompletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Car_Issu3",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Car_Issu3");
+        }
+    }
+}
