8417ed0769777421c4b0c9497301797af8f32b8d
williamjseim <33179905+williamjseim@users.noreply.github.com>
2024-08-26 12:19:31 +0200

    added an anonymous key to carissue so costumer can  see the issue without loggin in

---
+++ Backend/HackGame.Api/Migrations/20240826101851_anonymouskey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mechanic.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class anonymouskey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AnonymousUserKey",
+                table: "Car_Issu3",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnonymousUserKey",
+                table: "Car_Issu3");
+        }
+    }
+}
